187c5ef5f05d3e9ed1eaf632648186641532f257
cubamex209 <omarh209@gmail.com>
2018-03-16 12:24:07 -0700

    updating user routes

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-// server.js
-// SERVER-SIDE JAVASCRIPT
-
-
-/////////////////////////////
-//  SETUP and CONFIGURATION
-/////////////////////////////
-
 var express = require('express');
 var app = express();
 
@@ -15,11 +7,7 @@ app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-var db = require('./models'),
-  Card = db.Card,
-  Destination = db.Destination,
-  User = db.User;
-
+var db = require('./models');
 app.use(function(req, res, next) {
  res.header("Access-Control-Allow-Origin", "*");
  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -27,46 +15,7 @@ app.use(function(req, res, next) {
 });
 
 
-////////////////////
-//  ROUTES
-///////////////////
 
-// set view engine to ejs
-app.set("view engine", "ejs");
-
-app.get("/", function(req, res) {
-  res.sendFile('index');
-});
-
-//----------------Routes------------->
-//View of all cards
-app.get("/api/cards", function(req, res){
-  Card.find({}, function(err, allCards){
-    if (err){
-      console.log(err);
-    } else {
-      res.render("cards", {cards: allCards, error: null});
-    }
-});
-});
-
-//Showpage for individual cards
-app.get("/api/cards/:id", function(req, res){
-  Card.findOne({_id: CardId}, function(err, foundCard){
-    console.log(foundCard);
-    if (err){
-      console.log(err);
-    } else{
-      foundCard.title = req.body.title || foundCard.title;
-      foundCard.description = req.body.description || foundCard.description;
-      foundCard.image = req.body.title || foundCard.image;
-    }
-  })
-});
-
-
-
-//-------------Server------------->
 app.listen(process.env.PORT || 3000, function(){
 
   console.log("listening..");
@@ -87,3 +36,7 @@ app.get('/signup', function(req, res){
     }
   };
 });
+
+app.post('/signup', function(req, res){
+
+})
